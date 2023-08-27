f21770fb80039d53410df2eefe441a47935b701d
Stanoje Radivojevic <stacia@192.168.1.2>
2023-08-27 23:10:09 +0200

    feat(component): pools page empty state-no pools

--- src/pages/PoolsPage/index.tsx
+++ src/pages/PoolsPage/index.tsx
@@ -1,7 +1,25 @@
+import Button from "../../components/atom/Button";
+import PoolsEmptyState from "../../components/molecule/poolsEmptyState";
+
 const PoolsPage = () => {
   return (
-    <div className="flex justify-center">
-      <h1>Pool list</h1>
+    <div className="flex items-center justify-center px-[116px]">
+      <div className="flex w-full max-w-[1280px] flex-col">
+        <div className="flex items-center justify-between px-6 py-8">
+          <div className="flex flex-col  gap-[4px] leading-[120%]">
+            <div className="font-unbounded-variable text-heading-5 font-[700] tracking-[.046px] text-text-color-header-light">
+              Pools
+            </div>
+            <div className="tracking-[.2px] text-text-color-body-light">Earn fees by providing liquidity.</div>
+          </div>
+          <div>
+            <Button onClick={() => console.log("create poll")} variant="primary" size="large">
+              New Position
+            </Button>
+          </div>
+        </div>
+        <PoolsEmptyState />
+      </div>
     </div>
   );
 };
