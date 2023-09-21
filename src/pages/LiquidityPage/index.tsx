d00c7acea09c6039209ad9f9eb2b1936b06acb3b
Nikola Grujičić <nikola.grujicic@mvpworkshop.co>
2023-09-21 16:09:16 +0200

    refactor(liquidity pool): removed code not used

--- src/pages/LiquidityPage/index.tsx
+++ src/pages/LiquidityPage/index.tsx
@@ -1,5 +1,4 @@
 import { useLocation } from "react-router-dom";
-import PoolLiquidity from "../../components/organism/PoolLiquidity";
 import { LiquidityPageType } from "../../app/types/enum";
 import AddPoolLiquidity from "../../components/organism/AddPoolLiquidity";
 import WithdrawPoolLiquidity from "../../components/organism/WithdrawPoolLiquidity";
@@ -13,7 +12,7 @@ const LiquidityPage = () => {
     } else if (location.state?.pageType === LiquidityPageType.removeLiquidity) {
       return <WithdrawPoolLiquidity />;
     } else {
-      return <PoolLiquidity />;
+      return <AddPoolLiquidity />;
     }
   };
 
