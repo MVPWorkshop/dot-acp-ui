d8cab4d799086943ac678d48a972428579b48467
Nikola Grujičić <nikola.grujicic@mvpworkshop.co>
2023-10-05 16:36:22 +0200

    refactor(swap validation): changed swap validation and refactored pool page

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,31 @@ import { AppStateProvider } from "./state";
 import useStateAndDispatch from "./app/hooks/useStateAndDispatch";
 import { connectWalletAndFetchBalance } from "./services/polkadotWalletServices";
 import LocalStorage from "./app/util/localStorage";
+import { createPoolCardsArray } from "./services/poolServices";
 
 const App: FC = () => {
   const { dispatch, state } = useStateAndDispatch();
-  const { api } = state;
+  const { api, pools, selectedAccount } = state;
 
   const walletConnected = LocalStorage.get("wallet-connected");
+
+  const updatePoolsCards = async () => {
+    if (api && selectedAccount && pools) {
+      await createPoolCardsArray(api, dispatch, pools, selectedAccount);
+    } else if (api && pools) {
+      await createPoolCardsArray(api, dispatch, pools);
+    }
+  };
 
   useEffect(() => {
     if (walletConnected && api) {
       connectWalletAndFetchBalance(dispatch, api, walletConnected);
     }
   }, [api]);
+
+  useEffect(() => {
+    updatePoolsCards();
+  }, [pools, selectedAccount]);
 
   return (
     <AppStateProvider state={state} dispatch={dispatch}>
--- src/pages/PoolsPage/index.tsx
+++ src/pages/PoolsPage/index.tsx
@@ -1,117 +1,21 @@
-import { ApiPromise } from "@polkadot/api";
-import { t } from "i18next";
-import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import useGetNetwork from "../../app/hooks/useGetNetwork";
 import { POOLS_ADD_LIQUIDITY } from "../../app/router/routes";
-import { LpTokenAsset, PoolCardProps } from "../../app/types";
-import { ActionType, ButtonVariants } from "../../app/types/enum";
-import { formatDecimalsFromToken } from "../../app/util/helper";
-import dotAcpToast from "../../app/util/toast";
-import NativeTokenIcon from "../../assets/img/dot-token.svg";
-import AssetTokenIcon from "../../assets/img/test-token.svg";
-import { ReactComponent as TokenIcon } from "../../assets/img/token-icon.svg";
 import Button from "../../components/atom/Button";
-import { getPoolReserves } from "../../services/poolServices";
+import { ButtonVariants } from "../../app/types/enum";
+import { ReactComponent as TokenIcon } from "../../assets/img/token-icon.svg";
 import { useAppContext } from "../../state";
 import PoolDataCard from "./PoolDataCard";
+import { t } from "i18next";
 
 const PoolsPage = () => {
-  const { state, dispatch } = useAppContext();
-  const { api, selectedAccount, pools, poolsCards, tokenBalances } = state;
+  const { state } = useAppContext();
+  const { selectedAccount, pools, poolsCards, tokenBalances } = state;
 
   const navigate = useNavigate();
-
-  const { nativeTokenSymbol } = useGetNetwork();
 
   const navigateToAddLiquidity = () => {
     navigate(POOLS_ADD_LIQUIDITY);
   };
-
-  const createPoolCardsArray = async () => {
-    const apiPool = api as ApiPromise;
-
-    try {
-      const poolCardsArray: PoolCardProps[] = [];
-
-      await Promise.all(
-        pools.map(async (pool: any) => {
-          const lpTokenId = pool?.[1]?.lpToken;
-
-          let lpToken = null;
-          if (selectedAccount?.address) {
-            const lpTokenAsset = await apiPool.query.poolAssets.account(lpTokenId, selectedAccount?.address);
-            lpToken = lpTokenAsset.toHuman() as LpTokenAsset;
-          }
-
-          if (pool?.[0]?.[1]?.interior?.X2) {
-            const poolReserve: any = await getPoolReserves(
-              apiPool,
-              pool?.[0]?.[1]?.interior?.X2?.[1]?.GeneralIndex?.replace(/[, ]/g, "")
-            );
-
-            if (poolReserve?.length > 0) {
-              const assetTokenMetadata: any = await apiPool.query.assets.metadata(
-                pool?.[0]?.[1]?.interior?.X2?.[1]?.GeneralIndex?.replace(/[, ]/g, "")
-              );
-
-              const assetTokenBalance = formatDecimalsFromToken(
-                poolReserve?.[1]?.replace(/[, ]/g, ""),
-                assetTokenMetadata.toHuman()?.decimals
-              );
-
-              const nativeTokenBalance = formatDecimalsFromToken(poolReserve?.[0]?.replace(/[, ]/g, ""), "12");
-
-              poolCardsArray.push({
-                name: `${nativeTokenSymbol}–${assetTokenMetadata.toHuman()?.symbol}`,
-                lpTokenAsset: lpToken ? lpToken : null,
-                lpTokenId: lpTokenId,
-                assetTokenId: pool?.[0]?.[1]?.interior?.X2?.[1]?.GeneralIndex?.replace(/[, ]/g, ""),
-                totalTokensLocked: {
-                  nativeToken: nativeTokenBalance.toFixed(3),
-                  nativeTokenIcon: NativeTokenIcon,
-                  assetToken: assetTokenBalance.toFixed(3),
-                  assetTokenIcon: AssetTokenIcon,
-                },
-              });
-            }
-          }
-        })
-      );
-
-      poolCardsArray.sort(function (a, b) {
-        if (a.name < b.name) {
-          return -1;
-        }
-        if (a.name > b.name) {
-          return 1;
-        }
-        return 0;
-      });
-
-      poolCardsArray.sort(function (a, b) {
-        if (a.lpTokenAsset === null) return 1;
-        if (b.lpTokenAsset === null) return -1;
-        return parseInt(a?.lpTokenAsset?.balance) - parseInt(b?.lpTokenAsset?.balance);
-      });
-
-      dispatch({ type: ActionType.SET_POOLS_CARDS, payload: poolCardsArray });
-    } catch (error) {
-      dotAcpToast.error(`Error fetching pools: ${error}`);
-    }
-  };
-
-  useEffect(() => {
-    let mount = true;
-
-    if (api && mount && pools) {
-      createPoolCardsArray();
-    }
-
-    return () => {
-      mount = false;
-    };
-  }, [api, pools, selectedAccount]);
 
   return (
     <div className="flex items-center justify-center px-28 pb-16">
