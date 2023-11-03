f2db28ccad5e54ceeea35118344131636ef9f9d5
markoMVP85 <113442664+markoMVP85@users.noreply.github.com>
2023-11-03 15:09:30 +0100

    Feature/tests (#294)
    * feat: init test
    
    * feat: update
    
    * feat: unit tests
    
    * feat: update
    
    * refactor: install packages again
    
    * refactor: remove test
    
    ---------
    
    Co-authored-by: markoMVP85 <marko.jovanovic@mvpworkshop.co>

--- src/app/hooks/useGetNetwork.ts
+++ src/app/hooks/useGetNetwork.ts
@@ -2,7 +2,7 @@ import { NETWORKS } from "../../networkConfig";
 import { NetworkKeys } from "../types/enum";
 
 const useGetNetwork = () => {
-  const network = import.meta.env.VITE_NETWORK_NAME as NetworkKeys;
+  const network = process.env.VITE_NETWORK_NAME as NetworkKeys;
   return NETWORKS[network];
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import svgr from "vite-plugin-svgr";
+import { defineConfig } from "vite";
+import EnvironmentPlugin from "vite-plugin-environment";
 import mkcert from "vite-plugin-mkcert";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   server: {
     https: true,
   },
-  plugins: [react(), svgr(), mkcert()],
+  plugins: [react(), svgr(), mkcert(), EnvironmentPlugin("all")],
 });
