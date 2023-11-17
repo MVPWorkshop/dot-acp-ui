fabc25c76f84876088110b262a46df4fec75c1f2
Zarko Jevtic <zarko.jevtic@mvpworkshop.co>
2023-11-17 09:58:07 +0100

    fix: kusama assethub url and withdraw timeout

--- src/networkConfig.ts
+++ src/networkConfig.ts
@@ -24,6 +24,6 @@ export const NETWORKS: Record<NetworkKeys, NetworkConfig> = {
     nativeTokenSymbol: "KSM",
     rpcUrl: "wss://kusama-asset-hub-rpc.polkadot.io/",
     parents: 1,
-    assethubSubscanUrl: "https://kusama.subscan.io/account/",
+    assethubSubscanUrl: "https://assethub-kusama.subscan.io/account/",
   },
 };
